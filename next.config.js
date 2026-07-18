81bca9aa0bc1ebcdb2fb4eae610d3dba260dca1b
Sérgio Lira <83613556+SPLIRA@users.noreply.github.com>
2026-07-18 00:33:08 -0300

    fix: allow local network origin in development

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  allowedDevOrigins: ['localhost', '127.0.0.1', '192.168.2.121'],
   turbopack: {
     root: __dirname,
   },
